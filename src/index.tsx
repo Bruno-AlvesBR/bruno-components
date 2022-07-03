c8dfba8912bd82bc74da7ff4cc52663468cbe6f3
Bruno-AlvesBR <brunoph.faces12@gmail.com>
2022-07-03 17:22:07 -0300

    feat: up default files

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+import { DefaultInput } from "./components/Input";
+import { Login } from "./components/Login";
+import { Card } from "./components/Card";
+import { DefaultCarrousel } from "./components/Carrousel";
+
+export {
+  Button,
+  DefaultInput,
+  Login,
+  Card,
+  DefaultCarrousel,
+};
---
+++ src/styles/theme.ts
@@ -0,0 +1,3 @@
+import { createTheme, adaptV4Theme } from "@mui/material/styles";
+
+export const theme = createTheme(adaptV4Theme({}));
